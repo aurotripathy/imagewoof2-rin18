23abfa7e0ec12e3684b10ca6835da98b6ca62102
Auro Tripathy <auro@lightmatter.co>
2022-07-17 17:54:01 -0700

    add chk-pt cb and zoom-range & rot_range

--- rn18-imagewoof.py
+++ rn18-imagewoof.py
@@ -4,6 +4,7 @@ from keras.preprocessing.image import ImageDataGenerator
 import os
 from rn18_model import ResNet18
 import numpy as np
+from keras.callbacks import ModelCheckpoint
 import logging
 
 logger = tf.get_logger()
@@ -15,12 +16,14 @@ train_folder = os.path.join(root_folder, "imagewoof2-320/train/")
 val_folder = os.path.join(root_folder, "imagewoof2-320/val/")
 image_size = (320, 320, 3)
 target_size = (224, 224)
-nb_epochs = 40
+nb_epochs = 50
 lr = 0.1
 nb_classes = 10
 
 imagegen = ImageDataGenerator(featurewise_center=True,
                               featurewise_std_normalization=True,
+                              zoom_range=[0.8, 1.2],
+                              rotation_range=20,
                               horizontal_flip=True,)
 imagegen.mean = np.array([123.68, 116.779, 103.939], dtype=np.float32).reshape((1,1,3)) # ordering: [R, G, B]
 imagegen.std = 64.
@@ -52,12 +55,21 @@ sgd_optimizer = tf.keras.optimizers.SGD(
   
 adam_optimizer = keras.optimizers.Adam(learning_rate=lr, decay=1e-4)
 
-lr_schedule_callback = tf.keras.callbacks.LearningRateScheduler(scheduler_2)
+lr_schedule_cb = tf.keras.callbacks.LearningRateScheduler(scheduler_2)
+
+filepath = os.path.join('checkpoint',
+                        'rn18-best-epoch-{epoch:02d}-acc-{val_accuracy:.3f}.hdf5')
+checkpoint_cb = ModelCheckpoint(filepath=filepath,
+                                monitor='val_accuracy',
+                                verbose=1,
+                                save_best_only=True,
+                                save_weights_only=True,
+                                mode='max')
 
 model.compile(loss='categorical_crossentropy',
               optimizer=sgd_optimizer,
               metrics=['accuracy'])
 
 model.fit_generator(train, epochs=nb_epochs,
-                    callbacks=[lr_schedule_callback],
+                    callbacks=[lr_schedule_cb, checkpoint_cb],
                     validation_data=val)
